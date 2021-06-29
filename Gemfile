51d261a4aa2b5251088210b960c147053d62befd
Bess Sadler <bess.sadler@princeton.edu>
2021-06-29 17:51:26 -0400

    Fetch and parse data from OAI

--- Gemfile
+++ Gemfile
@@ -42,4 +42,5 @@ group :test do
   gem 'capybara', '>= 3.26'
   gem 'selenium-webdriver'
   gem 'webdrivers'
+  gem 'webmock'
 end
--- spec/lib/fetcher_spec.rb
+++ spec/lib/fetcher_spec.rb
@@ -2,12 +2,32 @@
 
 RSpec.describe Fetcher do
   let(:server) { 'https://dataspace-dev.princeton.edu/rest/' }
-  let(:community) { '85' } # Astrophysical Sciences
-  let(:fetcher) { described_class.new(server: server, community: community) }
+  let(:collection_id) { '85' } # Astrophysical Sciences
+  let(:fetcher) { described_class.new(server: server) }
 
-  it 'has a server and a community to harvest from' do
+  it 'has a server to harvest from' do
     expect(fetcher).to be_instance_of(described_class)
     expect(fetcher.server).to eq server
-    expect(fetcher.community).to eq community
+  end
+
+  context 'fetching data' do
+    let(:oai_feed) { File.read(File.join(fixture_path, 'astrophysical_sciences.json')) }
+
+    before do
+      stub_request(:get, "https://dataspace-dev.princeton.edu/rest//collections/85/items?expand=metadata&limit=100&offset=0")
+        .with(
+          headers: {
+            'Accept' => '*/*',
+            'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+            'User-Agent' => 'Faraday v1.4.3'
+          }
+        )
+        .to_return(status: 200, body: oai_feed, headers: {})
+    end
+
+    it 'fetches metadata for a given community' do
+      datasets = fetcher.fetch_collection(collection_id)
+      expect(datasets.size).to eq 39
+    end
   end
 end
