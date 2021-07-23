69a9723b78b7fd351714a782a8650ff2adec02f6
Bess Sadler <bess.sadler@princeton.edu>
2021-07-23 13:26:18 -0400

    Deploy to production and staging
    Move traject config out of auto-loaded directory

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,2 +1,2 @@
 # frozen_string_literal: true
-server "pdc-discovery-dev1.princeton.edu", user: "deploy", roles: %w[app db web]
+server "pdc-discovery-staging1.princeton.edu", user: "deploy", roles: %w[app db web]
