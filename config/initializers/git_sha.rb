77ec135c7a8f225042b9dcdfa144280b8a3ff0a0
Bess Sadler <bess.sadler@princeton.edu>
2021-08-11 13:20:42 -0400

    Get SHA from revisions.log

--- config/initializers/git_sha.rb
+++ config/initializers/git_sha.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
-revisions_logfile = "/opt/#{ENV['PROJECT_NAME']}/revisions.log"
+revisions_logfile = "/opt/pdc_discovery/revisions.log"
 
 GIT_SHA =
-  if Rails.env.production? && File.exist?(revisions_logfile)
+  if (Rails.env.production? || Rails.env.staging?) && File.exist?(revisions_logfile)
     `tail -1 #{revisions_logfile}`.chomp.split(" ")[3].gsub(/\)$/, '')
   elsif Rails.env.development? || Rails.env.test?
     `git rev-parse HEAD`.chomp
@@ -11,7 +11,7 @@ GIT_SHA =
   end
 
 BRANCH =
-  if Rails.env.production? && File.exist?(revisions_logfile)
+  if (Rails.env.production? || Rails.env.staging?) && File.exist?(revisions_logfile)
     `tail -1 #{revisions_logfile}`.chomp.split(" ")[1]
   elsif Rails.env.development? || Rails.env.test?
     `git rev-parse --abbrev-ref HEAD`.chomp
@@ -20,7 +20,7 @@ BRANCH =
   end
 
 LAST_DEPLOYED =
-  if Rails.env.production? && File.exist?(revisions_logfile)
+  if (Rails.env.production? || Rails.env.staging?) && File.exist?(revisions_logfile)
     deployed = `tail -1 #{revisions_logfile}`.chomp.split(" ")[7]
     Date.parse(deployed).strftime("%d %B %Y")
   else
--- spec/system/front_page_spec.rb
+++ spec/system/front_page_spec.rb
@@ -3,6 +3,7 @@
 describe 'Application landing page', type: :system do
   it "has a footer with latest deploy information" do
     visit '/'
+    byebug
     expect(page).to have_content "last updated"
   end
 end
