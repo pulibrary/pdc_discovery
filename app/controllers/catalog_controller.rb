fa7dcbc582ddaf548c334cfc1915d11e4b11a545
Bess Sadler <bess.sadler@princeton.edu>
2021-08-12 11:49:56 -0400

    Remove redundant title from show page

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -112,7 +112,6 @@ class CatalogController < ApplicationController
 
     # solr fields to be displayed in the show (single result) view
     #   The ordering of the field names is the order of the display
-    # config.add_show_field 'title_tsim', label: 'Title'
     config.add_show_field 'title_vern_ssim', label: 'Title'
     config.add_show_field 'subtitle_tsim', label: 'Subtitle'
     config.add_show_field 'subtitle_vern_ssim', label: 'Subtitle'
