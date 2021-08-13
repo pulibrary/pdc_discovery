ff96235e235f07c88fd9cfa081ff14d7320d3271
jrgriffiniii <jrgriffiniii@gmail.com>
2021-08-13 11:28:36 -0400

    [WIP]

---
+++ app/javascript/components/authors.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="document-authors">
+    <template v-for="author in authors">
+      <a href="#" class="document-author">{{ author }}</a>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    values: {
+      type: Array,
+      default: [],
+    },
+  },
+  computed: {
+    authors: function () {
+      return this.values.map((value, i) => {
+        return value + (i < this.values.length - 1 ? ", " : "");
+      });
+    },
+  },
+};
+</script>
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,11 +13,24 @@ import "styles/application";
 
 Vue.use(system);
 
+import Authors from "components/authors";
+import IssuedDate from "components/issued_date";
+import Abstract from "components/abstract";
+import Description from "components/description";
+import Methods from "components/methods";
+
 document.addEventListener("DOMContentLoaded", () => {
   var elements = document.getElementsByClassName("lux");
   for (var i = 0; i < elements.length; i++) {
     new Vue({
       el: elements[i],
+      components: {
+        authors: Authors,
+        "issued-date": IssuedDate,
+        abstract: Abstract,
+        description: Description,
+        methods: Methods,
+      },
     });
   }
 });
