61eb5b5aeb43b4edbb6b0dbcf37b3d94c789b888
Bess Sadler <bess.sadler@princeton.edu>
2021-08-17 15:02:27 -0400

    Automatically reindex research data after deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,3 +8,16 @@ set :repo_url, "https://github.com/pulibrary/pdc_discovery.git"
 set :branch, ENV["BRANCH"] || "main"
 
 set :deploy_to, "/opt/pdc_discovery"
+
+namespace :pdc_discovery do
+  desc "Reindex research data"
+  task :reindex do
+    on roles(:web) do
+      within release_path do
+        execute("cd #{release_path} && bundle exec rake index:research_data")
+      end
+    end
+  end
+end
+
+after "deploy:published", "pdc_discovery:reindex"
