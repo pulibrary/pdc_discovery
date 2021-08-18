d924d8e4f05f28e842788fc3073ed32427794cfc
Bess Sadler <bess.sadler@princeton.edu>
2021-08-18 16:56:48 -0400

    Index all date fields (#60)

--- spec/lib/date_normalizer_spec.rb
+++ spec/lib/date_normalizer_spec.rb
@@ -3,6 +3,7 @@
 RSpec.describe DateNormalizer do
   let(:years) { ['2015'] }
   let(:months_and_years) { ['2015-08'] }
+  let(:timestamps) { ['2015-08-18T18:14:22Z'] }
 
   describe "#format_array_for_display" do
     it "formats four digit years" do
@@ -13,6 +14,11 @@ RSpec.describe DateNormalizer do
     it "formats months and years" do
       formatted_dates = described_class.format_array_for_display(months_and_years)
       expect(formatted_dates.first).to eq "August 2015"
+    end
+
+    it "formats ISO-8601 timestamps" do
+      formatted_dates = described_class.format_array_for_display(timestamps)
+      expect(formatted_dates.first).to eq "18 August 2015"
     end
   end
 end
