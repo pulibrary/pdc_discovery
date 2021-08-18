2eec15e4c03000b28eef99c0f4727a9fc1580ef8
jrgriffiniii <jrgriffiniii@gmail.com>
2021-08-17 23:45:04 -0400

    [WIP] Introducing the footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def render_document_heading; end
+  include PdcDiscovery::LayoutHelperBehavior
 
-  def debug
-    blacklight_config.view_config(:show, action_name: action_name).document_presenter_class
-  end
+  def render_document_heading; end
 end
