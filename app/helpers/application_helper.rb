82bd5a742b99f8790cc096ee3e33bd78645d2c17
Hector Correa <hector_correa@princeton.edu>
2021-11-18 13:27:27 -0500

    Rubocop nit picking

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,10 +4,12 @@ module ApplicationHelper
   def render_document_heading; end
 
   # Outputs the HTML to render a single value as an HTML table row
+  # rubocop:disable Rails/OutputSafety
   def render_field_row(title, value, render_empty: false)
     return if value.nil?
     return if value.empty? && render_empty == false
-    value_encoded =html_escape(value)
+    value = "hello <b>world</b>"
+    value_encoded = html_escape(value)
     html = <<-HTML
     <tr>
       <th scope="row"><span>#{title}</span></th>
@@ -16,4 +18,5 @@ module ApplicationHelper
     HTML
     html.html_safe
   end
+  # rubocop:enable Rails/OutputSafety
 end
