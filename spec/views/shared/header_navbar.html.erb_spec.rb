d174ec4df3b3057ad178c75bab24a8dba6c51790
Kate Lynch <katelynch@princeton.edu>
2021-11-18 10:11:14 -0500

    Front page and header theming (#88)
    * Added style and images for header and homepage
    
    * Show searchbar in nav when not on homepage
    
    * Added test for search visibility in navbar
    
    * Rubocop and lint fixes

---
+++ spec/views/shared/header_navbar.html.erb_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+describe 'catalog/index', type: :system do
+  context 'when on the hompage' do
+    it 'does not show the navbar search' do
+      visit '/'
+      expect(page).not_to have_selector('div.navbar-search')
+    end
+  end
+
+  context 'when performing a search' do
+    it 'shows the navbar search' do
+      visit '/?search_field=all_fields&q=abc'
+      expect(page).to have_selector('div.navbar-search')
+    end
+  end
+
+  context 'when performing an empty search' do
+    it 'shows the navbar search' do
+      visit '/?search_field=all_fields&q='
+      expect(page).to have_selector('div.navbar-search')
+    end
+  end
+end
