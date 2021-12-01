13228a2fde7c22bebfbf505544be614be73aed5b
Hector Correa <hector_correa@princeton.edu>
2021-12-01 16:05:52 -0500

    yarn lint requirement

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,6 @@
 //= require bootstrap
 //= require blacklight/blacklight
 
-
 // For blacklight_range_limit built-in JS, if you don't want it you don't need
 // this:
 //= require 'blacklight_range_limit'
-
