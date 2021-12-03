4c02d702f33b1c3f2b326ca697dfa9a6ac7b9cb4
Hector Correa <hector_correa@princeton.edu>
2021-12-03 15:06:08 -0500

    Attempt to get version information from a local file (REVISIONS) rather than a file outside the app (revisions.log)

--- config/initializers/git_sha.rb
+++ config/initializers/git_sha.rb
@@ -3,8 +3,8 @@
 revisions_logfile = Rails.root.join("..", "..", "revisions.log")
 
 GIT_SHA =
-  if File.exist?(revisions_logfile)
-    `tail -1 #{revisions_logfile}`.chomp.split(" ")[3].gsub(/\)$/, '')
+  if File.exists?("REVISION")
+    File.read("REVISION").chomp.gsub(/\)$/, '')
   elsif Rails.env.development? || Rails.env.test?
     `git rev-parse HEAD`.chomp
   else
@@ -13,7 +13,13 @@ GIT_SHA =
 
 BRANCH =
   if File.exist?(revisions_logfile)
-    `tail -1 #{revisions_logfile}`.chomp.split(" ")[1]
+    revisions_line = `tail -1 #{revisions_logfile}`.chomp
+    revisions_sha = revisions_line.split(" ")[3]
+    if revisions_sha != GIT_SHA
+      "(stale)"
+    else
+      revisions_line.split(" ")[1]
+    end
   elsif Rails.env.development? || Rails.env.test?
     `git rev-parse --abbrev-ref HEAD`.chomp
   else
@@ -22,8 +28,12 @@ BRANCH =
 
 LAST_DEPLOYED =
   if File.exist?(revisions_logfile)
-    deployed = `tail -1 #{revisions_logfile}`.chomp.split(" ")[7]
-    Date.parse(deployed).strftime("%d %B %Y")
+    deployed_dir = Dir.getwd.split("/").last
+    if deployed_dir.start_with?(/\d\d\d\d\d\d\d\d/)
+      Date.parse(deployed_dir).strftime("%d %B %Y")
+    else
+      "N/A"
+    end
   else
     "Not in deployed environment"
   end
