08d3259ff5d14e99cfc8737ca74afb8c0d2c4ce3
Hector Correa <hector_correa@princeton.edu>
2021-12-08 12:26:35 -0500

    Rubocop dance

--- spec/models/version_footer_spec.rb
+++ spec/models/version_footer_spec.rb
@@ -4,26 +4,35 @@ require 'rails_helper'
 
 RSpec.describe VersionFooter do
   describe "info" do
-    it "detects stale information" do
-      described_class.revision_file = Pathname.new(fixture_path).join("REVISION").to_s
-      described_class.revisions_logfile = Pathname.new(fixture_path).join("revisions_stale.log").to_s
-      described_class.reset!
-      info = described_class.info
-      expect(info[:stale]).to be true
-      expect(info[:sha]).to eq "2222ae5c4ad9aaa0faad5208f1bf8108bd5934bf"
-      expect(info[:branch]).to eq "version-2"
-      expect(info[:version]).to eq "02 December 2021"
+    context "with stale information" do
+      before do
+        described_class.revision_file = Pathname.new(fixture_path).join("REVISION").to_s
+        described_class.revisions_logfile = Pathname.new(fixture_path).join("revisions_stale.log").to_s
+        described_class.reset!
+      end
+
+      it "detects stale information" do
+        info = described_class.info
+        expect(info[:stale]).to be true
+        expect(info[:sha]).to eq "2222ae5c4ad9aaa0faad5208f1bf8108bd5934bf"
+        expect(info[:branch]).to eq "version-2"
+        expect(info[:version]).to eq "02 December 2021"
+      end
     end
 
-    it "detects current information" do
-      described_class.revision_file = Pathname.new(fixture_path).join("REVISION").to_s
-      described_class.revisions_logfile = Pathname.new(fixture_path).join("revisions_current.log").to_s
-      described_class.reset!
-      info = described_class.info
-      expect(info[:stale]).to be false
-      expect(info[:sha]).to eq "4443ae5c4ad9aaa0faad5208f1bf8108bd5934bf"
-      expect(info[:branch]).to eq "version-3"
-      expect(info[:version]).to eq "03 December 2021"
+    context "with current information" do
+      before do
+        described_class.revision_file = Pathname.new(fixture_path).join("REVISION").to_s
+        described_class.revisions_logfile = Pathname.new(fixture_path).join("revisions_current.log").to_s
+        described_class.reset!
+      end
+      it "detects current information" do
+        info = described_class.info
+        expect(info[:stale]).to be false
+        expect(info[:sha]).to eq "4443ae5c4ad9aaa0faad5208f1bf8108bd5934bf"
+        expect(info[:branch]).to eq "version-3"
+        expect(info[:version]).to eq "03 December 2021"
+      end
     end
   end
 end
