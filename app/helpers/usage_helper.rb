4704b11c256258f25ad16704d3fadea4f9e7b52a
Kate Lynch <katelynch@princeton.edu>
2022-01-20 15:59:05 -0500

    Pulling pageviews

--- app/helpers/usage_helper.rb
+++ app/helpers/usage_helper.rb
@@ -6,6 +6,10 @@ module UsageHelper
   end
 
   def views
-    "12"
+    c = PlausibleApi::Client.new('pdc-discovery-staging.princeton.edu', (ENV['PLAUSIBLE_KEY'] || ''))
+    c.aggregate({ date: "2021-01-01,#{Date.today.strftime("%Y-%m-%d")}", metrics: 'visitors,pageviews', filters: "event:page==/catalog/#{@document.id}"})["pageviews"]["value"]
+  rescue => e
+    logger.error "PLAUSIBLE ERROR: #{e.message}"
+    '0'
   end
 end
