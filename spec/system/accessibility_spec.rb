b3b750194665be7aee9126b98f767ed0adb2ab67
Kate Lynch <katelynch@princeton.edu>
2022-01-26 09:49:22 -0500

    Added search results, about page, show view to accessibility tests

--- spec/system/accessibility_spec.rb
+++ spec/system/accessibility_spec.rb
@@ -2,7 +2,16 @@
 require "rails_helper"
 
 describe "accessibility", type: :system, js: true do
+
+  let(:community_fetch_with_expanded_metadata) { file_fixture("single_item.xml").read }
+  let(:indexer) do
+    Indexer.new(community_fetch_with_expanded_metadata)
+  end
+
   before do
+    Blacklight.default_index.connection.delete_by_query("*:*")
+    Blacklight.default_index.connection.commit
+    indexer.index
     stub_request(:get, "https://github.com/mozilla/geckodriver/releases/latest")
     .with(
       headers: {
@@ -14,8 +23,29 @@ describe "accessibility", type: :system, js: true do
     )
   end
   context "homepage" do
-    it "complies with ..." do
+    it "complies with WCAG 2.0 AA and Section 508" do
       visit "/"
+      expect(page).to be_axe_clean.according_to :wcag2aa, :section508
+    end
+  end
+
+  context "about page" do
+    it "complies with WCAG 2.0 AA and Section 508" do
+      visit "/about"
+      expect(page).to be_axe_clean.according_to :wcag2aa, :section508
+    end
+  end
+
+  context "search results page" do
+    it "complies with WCAG 2.0 AA and Section 508" do
+      visit '/?search_field=all_fields&q='
+      expect(page).to be_axe_clean.according_to :wcag2aa, :section508
+    end
+  end
+
+  context "single record page page" do
+    it "complies with WCAG 2.0 AA and Section 508" do
+      visit '/catalog/78348'
       expect(page).to be_axe_clean.according_to :wcag2aa, :section508
     end
   end
