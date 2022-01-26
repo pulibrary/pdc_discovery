78e92370463768b31b3fc63e72d058fef61fd36e
Kate Lynch <katelynch@princeton.edu>
2022-01-26 16:22:25 -0500

    First pass accessibility suite, ignoring false positives

--- spec/system/accessibility_spec.rb
+++ spec/system/accessibility_spec.rb
@@ -25,28 +25,40 @@ describe "accessibility", type: :system, js: true do
   context "homepage" do
     it "complies with WCAG 2.0 AA and Section 508" do
       visit "/"
-      expect(page).to be_axe_clean.according_to :wcag2aa, :section508
+      expect(page).to be_axe_clean
+        .according_to(:wcag2a, :wcag2aa, :wcag21a, :wcag21aa, :section508)
+        .skipping(:'color-contrast') # false positives
+        .excluding(".tt-hint") # Issue is in typeahead.js library
     end
   end
 
   context "about page" do
     it "complies with WCAG 2.0 AA and Section 508" do
       visit "/about"
-      expect(page).to be_axe_clean.according_to :wcag2aa, :section508
+      expect(page).to be_axe_clean
+        .according_to(:wcag2a, :wcag2aa, :wcag21a, :wcag21aa, :section508)
+        .skipping(:'color-contrast') # false positives
+        .excluding(".tt-hint") # Issue is in typeahead.js library
     end
   end
 
   context "search results page" do
     it "complies with WCAG 2.0 AA and Section 508" do
       visit '/?search_field=all_fields&q='
-      expect(page).to be_axe_clean.according_to :wcag2aa, :section508
+      expect(page).to be_axe_clean
+        .according_to(:wcag2a, :wcag2aa, :wcag21a, :wcag21aa, :section508)
+        .skipping(:'color-contrast') # false positives
+        .excluding(".tt-hint") # Issue is in typeahead.js library
     end
   end
 
   context "single record page page" do
     it "complies with WCAG 2.0 AA and Section 508" do
       visit '/catalog/78348'
-      expect(page).to be_axe_clean.according_to :wcag2aa, :section508
+      expect(page).to be_axe_clean
+        .according_to(:wcag2a, :wcag2aa, :wcag21a, :wcag21aa, :section508)
+        .skipping(:'color-contrast') # false positives
+        .excluding(".tt-hint") # Issue is in typeahead.js library
     end
   end
 end
