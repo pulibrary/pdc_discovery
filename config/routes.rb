9f335ff944baf16ed064e0bb6128c4737d360fc1
Bess Sadler <bess.sadler@princeton.edu>
2022-02-16 12:34:55 -0500

    Auto-generate sitemap to enable search engine crawling
    Implement blacklight_dynamic_sitemap so our sitemap stays current.
    Includes solr configuration changes that must be separately added to
    pul_solr before this will work in production.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@
 Rails.application.routes.draw do
   concern :range_searchable, BlacklightRangeLimit::Routes::RangeSearchable.new
   mount Blacklight::Engine => '/'
+  mount BlacklightDynamicSitemap::Engine => '/'
+
   concern :marc_viewable, Blacklight::Marc::Routes::MarcViewable.new
   root to: 'catalog#index'
   concern :searchable, Blacklight::Routes::Searchable.new
