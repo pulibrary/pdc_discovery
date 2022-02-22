4eeee9b06610353b56d3e40ea697be63ddcc0121
Hector Correa <hector_correa@princeton.edu>
2022-02-22 11:48:19 -0500

    Display authors separated by semicolons in search results

--- spec/system/search_results_page_spec.rb
+++ spec/system/search_results_page_spec.rb
@@ -13,7 +13,7 @@ describe 'Search Results Page', type: :system, js: true do
     visit '/?search_field=all_fields&q='
 
     expect(page).to have_link('Midplane neutral density profiles in the National Spherical Torus Experiment', href: '/catalog/78348')
-    expect(page).to have_content 'Stotler, D., F. Scotti, R.E. Bell, A. Diallo' # authors
+    expect(page).to have_content 'Stotler, D.; F. Scotti; R.E. Bell; A. Diallo' # authors
     expect(page).to have_content 'Atomic and molecular density data in the outer midplane of NSTX' # abstract
   end
 
