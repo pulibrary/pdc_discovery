6c279330017243b66aa31b54f297388507e7003f
Hector Correa <hector_correa@princeton.edu>
2022-03-01 12:47:34 -0500

    Switch the property used to log downloads so that we can combine stats for file downloads with globus downloads with a single API call filtered by page.

--- app/assets/javascripts/plausible.js
+++ app/assets/javascripts/plausible.js
@@ -1,9 +1,9 @@
 function log_plausible_file_download(filename) {
   console.log("log_plausible_file_download: " + filename);
-  plausible("Download", { props: { info: filename } });
+  plausible("Download", { props: { filename: filename } });
 }
 
 function log_plausible_globus_download(id) {
   console.log("log_plausible_globus_download: " + id);
-  plausible("GlobusDownload", { props: { info: id } });
+  plausible("Download", { props: { filename: "globus-download" } });
 }
--- app/models/plausible.rb
+++ app/models/plausible.rb
@@ -2,19 +2,32 @@
 require 'plausible_api'
 
 class Plausible
+  PLAUSIBLE_URL = 'https://plausible.io/api/v1'
+
   def self.page_views(document_id)
     # byebug
     # return 'X' unless Rails.env.staging? || Rails.env.production?
-
     c = PlausibleApi::Client.new(Rails.configuration.pdc_discovery.plausible_site_id, (ENV['PLAUSIBLE_KEY'] || ''))
     response = c.aggregate({ date: "2021-01-01,#{Time.zone.today.strftime('%Y-%m-%d')}", metrics: 'visitors,pageviews', filters: "event:page==/catalog/#{document_id}" })
     response["pageviews"]["value"]
   rescue => e
-    logger.error "PLAUSIBLE ERROR: (Document: #{document_id}) #{e.message}"
+    Rails.logger.error "PLAUSIBLE ERROR: (Pageviews for document: #{document_id}) #{e.message}"
     Honeybadger.notify(e.message)
     '0'
   end
 
   def self.downloads(document_id)
+    site_id = Rails.configuration.pdc_discovery.plausible_site_id
+    period = "7d"
+    page = "/catalog/#{document_id}"
+    url = "#{PLAUSIBLE_URL}/stats/breakdown?site_id=#{site_id}&period=#{period}&property=event:props:filename&filters=event:page==#{page}&metrics=visitors,pageviews"
+    authorization = "Bearer #{ENV['PLAUSIBLE_KEY']}"
+    response = HTTParty.get(sub_community_url, headers: { 'Authorization' => authorization})
+    Rails.logger.info response.to_s
+    '123'
+  rescue => e
+    Rails.logger.error "PLAUSIBLE ERROR: (Downloads for document: #{document_id}) #{e.message}"
+    Honeybadger.notify(e.message)
+    '0'
   end
 end
