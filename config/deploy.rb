66d28273af72fe473cf7ff77ef5039ecc4e3b21b
Bess Sadler <bess.sadler@princeton.edu>
2022-03-02 11:21:04 -0500

    Do not lose logs on deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,8 @@ lock "~> 3.16.0"
 set :application, "pdc_discovery"
 set :repo_url, "https://github.com/pulibrary/pdc_discovery.git"
 
+set :linked_dirs, %w(log public/system public/assets)
+
 # Default branch is :main
 set :branch, ENV["BRANCH"] || "main"
 
