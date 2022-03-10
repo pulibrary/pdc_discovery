f71917e43251093669ff3be16bd52edb4e81017a
Hector Correa <hector_correa@princeton.edu>
2022-03-10 13:28:54 -0500

    New content for About page

--- spec/views/home/about.html.erb_spec.rb
+++ spec/views/home/about.html.erb_spec.rb
@@ -2,6 +2,6 @@
 describe '/about', type: :system do
   it 'renders the about page' do
     visit '/about'
-    expect(page).to have_text('Lorem ipsum')
+    expect(page).to have_text("About Princeton's Research Data Repository")
   end
 end
