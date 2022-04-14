c3acba810338fc86d598afd30716a7980b6d67ea
Bess Sadler <bess.sadler@princeton.edu>
2022-04-14 15:57:35 -0400

    Fix capistrano version (#265)

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.16.0"
+lock "~> 3.17.0"
 
 set :application, "pdc_discovery"
 set :repo_url, "https://github.com/pulibrary/pdc_discovery.git"
