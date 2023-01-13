52be278ccd3f342307e5a89e74eb73fc703fc5e2
Hector Correa <hector_correa@princeton.edu>
2023-01-13 16:01:10 -0500

    Adjust a few more tests to use the new fixtures. Deleted spec/lib/pdc_describe_indexer_spec.rb now that its functionality has been moved to spec/lib/describe_indexer_spec.rb. Deleted old fixtures.

--- spec/lib/describe_indexer_spec.rb
+++ spec/lib/describe_indexer_spec.rb
@@ -2,7 +2,7 @@
 
 RSpec.describe DescribeIndexer do
   describe 'indexing a single record' do
-    let(:single_item) { file_fixture("bitklavier.json").read }
+    let(:single_item) { file_fixture("bitklavier_binaural.json").read }
     let(:indexer) do
       described_class.new(rss_url: "file://whatever.rss")
     end
@@ -103,4 +103,49 @@ RSpec.describe DescribeIndexer do
       end
     end
   end
+
+  describe 'indexing an RSS feed from PDC Describe' do
+    let(:rss_feed) { file_fixture("works.rss").read }
+    let(:resource1) { file_fixture("bitklavier_binaural.json").read }
+    let(:resource2) { file_fixture("sowing_the_seeds.json").read }
+    let(:rss_url_string) { "https://pdc-describe-prod.princeton.edu/describe/works.rss" }
+    let(:indexer) { described_class.new(rss_url: rss_url_string) }
+
+    it "has a traject indexer" do
+      expect(indexer.traject_indexer).to be_instance_of Traject::Indexer::NokogiriIndexer
+    end
+
+    context 'indexing to solr' do
+      before do
+        Blacklight.default_index.connection.delete_by_query("*:*")
+        Blacklight.default_index.connection.commit
+        stub_request(:get, "https://pdc-describe-prod.princeton.edu/describe/works.rss")
+          .to_return(status: 200, body: rss_feed, headers: {})
+        stub_request(:get, "https://pdc-describe-prod.princeton.edu/describe/works/6.json")
+          .to_return(status: 200, body: resource1, headers: {})
+        stub_request(:get, "https://pdc-describe-prod.princeton.edu/describe/works/20.json")
+          .to_return(status: 200, body: resource2, headers: {})
+      end
+
+      it "sends items to solr" do
+        response = Blacklight.default_index.connection.get 'select', params: { q: '*:*' }
+        expect(response["response"]["numFound"]).to eq 0
+
+        # If index_pdc_describe == false, do not index PDC Describe.
+        # This is a safety measure so we don't index in production until we're ready
+        # See config/pdc_discovery.yml to change this setting for real
+        Rails.configuration.pdc_discovery.index_pdc_describe = false
+        indexer.index
+        response = Blacklight.default_index.connection.get 'select', params: { q: '*:*' }
+        expect(response["response"]["numFound"]).to eq 0
+
+        # If index_pdc_describe == true, DO index PDC Describe.
+        Rails.configuration.pdc_discovery.index_pdc_describe = true
+        indexer.index
+        response = Blacklight.default_index.connection.get 'select', params: { q: '*:*' }
+        expect(response["response"]["numFound"]).to eq 2
+      end
+    end
+  end
+
 end
