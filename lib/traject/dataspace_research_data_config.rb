38716ae540d24baca9c79f79c05f3f39ef84bcce
Hector Correa <hector_correa@princeton.edu>
2023-01-19 16:25:16 -0500

    Account for DOI (in addition to ARK)

--- lib/traject/dataspace_research_data_config.rb
+++ lib/traject/dataspace_research_data_config.rb
@@ -19,8 +19,7 @@ end
 
 each_record do |record, context|
   uris = record.xpath("/item/metadata/key[text()='dc.identifier.uri']/../value")
-  ark_uri = uris.find { |uri| uri.text.start_with?("http://arks.princeton.edu/ark:/") }&.text
-  if ImportHelper.pdc_describe_match_found?(ark_uri)
+  if ImportHelper.pdc_describe_match?(uris)
     id = record.xpath('/item/id')
     context.skip!("Skipping DataSpace record #{id} - already imported from PDC Describe")
   end
