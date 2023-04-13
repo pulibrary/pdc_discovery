87801581d82dac4828b50724be8cb3d8e62b03fa
Hector Correa <hector_correa@princeton.edu>
2023-04-13 10:18:04 -0400

    Configures one prod server with the reindex role. Makes sure the reindex job is scheduled on the correct role

--- config/schedule.rb
+++ config/schedule.rb
@@ -23,6 +23,6 @@
 # Sets the PATH environment variable and run the job
 set :job_template, "bash -l -c 'export PATH=\"/usr/local/bin/:$PATH\" && :job'"
 
-every :day, at: '12:20am', roles: [:app] do
+every :day, at: '12:20am', roles: [:reindex] do
   rake "index:research_data"
 end
