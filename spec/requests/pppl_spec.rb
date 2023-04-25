848155b06774a83c40da607dd96b65ba8afe86e3
Bess Sadler <bess.sadler@princeton.edu>
2023-04-25 13:56:35 -0400

    Add more PPPL fixtures so we can test pagination

--- spec/requests/pppl_spec.rb
+++ spec/requests/pppl_spec.rb
@@ -4,6 +4,9 @@ require 'rails_helper'
 RSpec.describe "PPPL has a harvest endpoint", type: :request do
   let(:pppl1) { File.read(File.join(fixture_path, 'files', 'pppl1.json')) }
   let(:pppl2) { File.read(File.join(fixture_path, 'files', 'pppl2.json')) }
+  let(:pppl3) { File.read(File.join(fixture_path, 'files', 'pppl3.json')) }
+  let(:pppl4) { File.read(File.join(fixture_path, 'files', 'pppl4.json')) }
+
   let(:other_data) { File.read(File.join(fixture_path, 'files', 'bitklavier_binaural.json')) }
   let(:indexer) { DescribeIndexer.new }
 
@@ -12,6 +15,8 @@ RSpec.describe "PPPL has a harvest endpoint", type: :request do
     Blacklight.default_index.connection.commit
     indexer.index_one(pppl1)
     indexer.index_one(other_data)
+    indexer.index_one(pppl3)
+    indexer.index_one(pppl4)
     # Note that pppl2 should have the most recent timestamp, since it was indexed last
     indexer.index_one(pppl2)
   end
@@ -22,9 +27,16 @@ RSpec.describe "PPPL has a harvest endpoint", type: :request do
     expect(response).to have_http_status(:success)
     results = JSON.parse(response.body)
     # There should be 3 records in the index, but only 2 of them are from PPPL
-    expect(results.count).to eq 2
+    expect(results.count).to eq 4
     # The most recently indexed item (pppl2) should be first
     first_doi_url = JSON.parse(results.first["pdc_describe_json_ss"])["resource"]["doi"]
     expect(first_doi_url).to match(JSON.parse(pppl2)["resource"]["doi"])
   end
+
+  # it "can paginate through multiple pages of responses" do
+  #   data = JSON.parse(pppl_data)
+  #   byebug
+  #   get "/pppl_reporting_feed.json"
+  #   expect(response).to have_http_status(:success)
+  # end
 end
