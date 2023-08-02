0f504666010c05ee81099628062181bdeffe21b5
Hector Correa <hector_correa@princeton.edu>
2023-08-02 14:57:53 -0400

    Index a specific field for the Globus URL to the folder of the files in PDC Describe records (#463)

--- spec/lib/traject/import_helper_spec.rb
+++ spec/lib/traject/import_helper_spec.rb
@@ -22,4 +22,12 @@ RSpec.describe ImportHelper do
       expect(described_class.display_filename("10.123/4567/x40/file1.txt", "10.123/4567")).to eq "10.123/4567/x40/file1.txt"
     end
   end
+
+  describe "#globus_folder_uri_from_file" do
+    it "returns the URI with the origin and path parameters" do
+      globus_folder_uri = described_class.globus_folder_uri_from_file("10.123/4567/40/file1.txt")
+      expect(globus_folder_uri.include?("origin_id=xxxx-yyyy-zzzz-aaaa-bbbb")).to be true
+      expect(globus_folder_uri.include?("origin_path=%2Fpdc-describe-testing-postcuration%2F10.123%2F4567%2F40%2F")).to be true
+    end
+  end
 end
