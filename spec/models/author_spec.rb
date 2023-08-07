6a850bea3517f8a38a820a0a25b3aef0a3fb806b
Hector Correa <hector_correa@princeton.edu>
2023-08-07 17:03:36 -0400

    Added test for new class

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -4,10 +4,30 @@ require 'rails_helper'
 
 RSpec.describe Author do
   describe "#affiliation_name" do
-    let (:author_zero_affiliations) { { "value": "Smith, Jane", "given_name": "Jane", "family_name": "Smith" } }
-    it "x" do
-      author = described_class.new(author_zero_affiliations)
-      expect(doc.affiliation_name).to be nil
+    # Use => in this hash to force RSpec to use strings in the keys (rather than symbols) since the source
+    # data always uses strings
+    let (:author_no_affiliation) { { "value" => "Smith, Jane", "given_name" => "Jane", "family_name" => "Smith" } }
+    let (:author_with_affiliation) {
+      {
+        "value" => "Smith, Jane",
+        "given_name" => "Jane",
+        "family_name" => "Smith",
+        "affiliations" =>  [
+          {
+            "value" =>  "Princeton Plasma Physics Laboratory",
+            "identifier" =>  "https://ror.org/03vn1ts68",
+            "scheme" =>  "ROR"
+          }
+        ]
+      }
+    }
+
+    it "handles affiliation information" do
+      author = described_class.new(author_no_affiliation)
+      expect(author.affiliation_name).to be nil
+
+      author = described_class.new(author_with_affiliation)
+      expect(author.affiliation_name).to eq "Princeton Plasma Physics Laboratory"
     end
   end
 end
