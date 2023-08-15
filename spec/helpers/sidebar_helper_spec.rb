64cc1cf98bcc7fb5410673c88365850e4be4b9b8
Hector Correa <hector_correa@princeton.edu>
2023-08-15 10:52:50 -0400

    Rubocop nit picking

--- spec/helpers/sidebar_helper_spec.rb
+++ spec/helpers/sidebar_helper_spec.rb
@@ -2,11 +2,11 @@
 
 RSpec.describe SidebarHelper, type: :helper do
   describe "#render_sidebar_related_identifiers" do
-    let(:text_id) { {"related_identifier" => "123", "relation_type" => "IsCitedBy"}}
-    let(:link_id) { {"related_identifier" => "http://abc.org/999", "relation_type" => "IsCitedBy"}}
-    let(:bad_id1) { {"related_identifier" => nil, "relation_type" => "badid"}}
-    let(:bad_id2) { {"related_identifier" => "badid", "relation_type" => nil}}
-    let(:identifiers) { [text_id, link_id, bad_id1, bad_id2]}
+    let(:text_id) { { "related_identifier" => "123", "relation_type" => "IsCitedBy" } }
+    let(:link_id) { { "related_identifier" => "http://abc.org/999", "relation_type" => "IsCitedBy" } }
+    let(:bad_id1) { { "related_identifier" => nil, "relation_type" => "badid" } }
+    let(:bad_id2) { { "related_identifier" => "badid", "relation_type" => nil } }
+    let(:identifiers) { [text_id, link_id, bad_id1, bad_id2] }
     let(:html) { helper.render_sidebar_related_identifiers("Related Identifiers", identifiers) }
 
     it "titleize relation type" do
