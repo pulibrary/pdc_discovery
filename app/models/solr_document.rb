40e8a8bc9d1ac7f29f07160a19e785e03227dc5f
Bess Sadler <bess.sadler@princeton.edu>
2023-08-17 14:33:10 -0400

    Remove MaRC boilerplate bc we do not use MaRC (#481)

--- app/models/solr_document.rb
+++ app/models/solr_document.rb
@@ -3,12 +3,6 @@
 # rubocop:disable Metrics/ClassLength
 class SolrDocument
   include Blacklight::Solr::Document
-  # The following shows how to setup this blacklight document to display marc documents
-  extension_parameters[:marc_source_field] = :marc_ss
-  extension_parameters[:marc_format_type] = :marcxml
-  use_extension(Blacklight::Solr::Document::Marc) do |document|
-    document.key?(SolrDocument.extension_parameters[:marc_source_field])
-  end
 
   field_semantics.merge!(
     title: 'title_tesim',
