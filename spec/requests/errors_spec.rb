709ef71d6ee462cb81055b4ba57a4811d6704297
James R. Griffin III <1443986+jrgriffiniii@users.noreply.github.com>
2023-10-02 09:20:35 -0400

    Ensuring that there are view templates for 404 responses supporting non-HTML requests (#516)

--- spec/requests/errors_spec.rb
+++ spec/requests/errors_spec.rb
@@ -15,4 +15,36 @@ RSpec.describe "Errors", type: :request do
       expect(response).to have_http_status(:internal_server_error)
     end
   end
+
+  describe "GET /not_found.json" do
+    it "returns a 404 response code with an empty body" do
+      get "/errors/not_found.json"
+      expect(response).to have_http_status(:not_found)
+      expect(response.body).to be_empty
+    end
+  end
+
+  describe "GET /not_found.rss" do
+    it "returns a 404 response code with an empty body" do
+      get "/errors/not_found.rss"
+      expect(response).to have_http_status(:not_found)
+      expect(response.body).to be_empty
+    end
+  end
+
+  describe "GET /not_found.xml" do
+    it "returns a 404 response code with an empty body" do
+      get "/errors/not_found.xml"
+      expect(response).to have_http_status(:not_found)
+      expect(response.body).to be_empty
+    end
+  end
+
+  describe "GET /not_found.svg" do
+    it "returns a 404 response code with an html page" do
+      get "/errors/not_found.svg"
+      expect(response).to have_http_status(:not_found)
+      expect(response.body).not_to be_empty
+    end
+  end
 end
