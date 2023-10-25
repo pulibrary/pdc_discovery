fcfd72ceaab7a2279459170288c4584293d42062
carolyncole <1599081+carolyncole@users.noreply.github.com>
2023-10-25 09:43:25 -0400

    Adding a health monitor page (#527)
    Includes solr check

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  mount HealthMonitor::Engine, at: "/"
+
   concern :range_searchable, BlacklightRangeLimit::Routes::RangeSearchable.new
   mount Blacklight::Engine => '/'
   mount BlacklightDynamicSitemap::Engine => '/'
