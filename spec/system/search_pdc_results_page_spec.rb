848fc07c196407f5602c33f8351a7de98ab9a800
Jaymee Hyppolite <54775395+JaymeeH@users.noreply.github.com>
2024-03-07 09:03:29 -0500

    577 missing pub date (#578)
    * Adding year_available_itsi to pdc describe indexing configuration
    This allows the publisj date to be part of the facet and removes the [missing] from the page
    
    * added test to check for year published facet
    
    ---------
    
    Co-authored-by: Carolyn Cole <cac9@princeton.edu>

--- spec/system/search_pdc_results_page_spec.rb
+++ spec/system/search_pdc_results_page_spec.rb
@@ -33,5 +33,10 @@ describe 'Search Results PDC Page', type: :system, js: true do
     expect(page).to have_content("System Studies")
     click_on("System Studies")
     expect(page).to have_content("Fusion Pilot Plant performance and the role of a Sustained High Power Density tokamak")
+
+    click_on "Year Published"
+    expect(page).to have_content "View larger"
+    expect(page).not_to have_content "[Missing]"
+    expect(page).to have_content "2022\n2022"
   end
 end
