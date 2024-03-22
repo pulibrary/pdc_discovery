a26a8169e6a8f742ffc30b86f50a04c22307e3e9
leefaisonr <rl3667@princeton.edu>
2024-03-22 15:22:40 -0400

    removes extra text from front page

--- app/helpers/recently_added_helper.rb
+++ app/helpers/recently_added_helper.rb
@@ -7,7 +7,7 @@ module RecentlyAddedHelper
     <li id="recently-added-#{entry.id}">
     <span class="genre"><i id="#{entry.id}" class="bi #{entry.icon_css}"></i>#{html_escape(entry.genre)}</span>
     <span class="title">#{link_to(html_escape(entry.title), solr_document_path(entry.id))}</span>
-    <span class="credit">Published on #{html_escape(entry.issued_date)}, #{html_escape(entry.authors_et_al)}</span>
+    <span class="credit">Published #{html_escape(entry.issued_date)}, #{html_escape(entry.authors_et_al)}</span>
     </li>
     HTML
     html.html_safe
