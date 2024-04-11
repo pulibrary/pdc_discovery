1ae959092693d244c1c40e8e2e37df2b0cef4e29
Hector Correa <hector_correa@princeton.edu>
2024-04-11 14:42:53 -0400

    Adds a new class to define properties to export when requesting JSON for a dataset
    Co-authored-by: Claudia Lee <claudiawulee@users.noreply.github.com>

---
+++ app/models/document_export.rb
@@ -0,0 +1,9 @@
+class DocumentExport
+  attr_reader :id, :title, :files
+
+  def initialize(solr_document)
+    @id = solr_document.id
+    @title = solr_document.title
+    @files = solr_document.files
+  end
+end
