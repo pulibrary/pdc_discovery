4a02947fe71ef651c85c98b749651f3e321eee2e
Claudia Lee <claudialee57@gmail.com>
2024-04-16 14:35:03 -0400

    tests for DocumentExport

--- app/models/document_export.rb
+++ app/models/document_export.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 class DocumentExport
-  attr_reader :id, :title, :files
+  attr_reader :id, :title, :files, :description, :abstract
 
   def initialize(solr_document)
     @id = solr_document.id
     @title = solr_document.title
     @files = solr_document.files
+    @description = solr_document.description
+    @abstract = solr_document.abstract
   end
 end
