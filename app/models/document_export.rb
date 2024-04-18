034549bb519430fd20774eace3fcb0f378675118
Hector Correa <hector_correa@princeton.edu>
2024-04-18 14:41:13 -0400

    Added a few more properties for PDC Describe records
    Co-authored-by: Claudia Lee <claudiawulee@users.noreply.github.com>

--- app/models/document_export.rb
+++ app/models/document_export.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class DocumentExport
-  attr_reader :id, :title, :files, :description, :abstract
+  attr_reader :id, :title, :files, :description, :abstract, :rights, :authors, :doi_value, :doi_url,
+    :total_file_size, :embargo_date, :globus_url
 
   def initialize(solr_document)
     @id = solr_document.id
@@ -9,5 +10,12 @@ class DocumentExport
     @files = solr_document.files
     @description = solr_document.description
     @abstract = solr_document.abstract
+    @rights = solr_document.rights_enhanced
+    @authors = solr_document.authors_ordered
+    @doi_value = solr_document.doi_value
+    @doi_url = solr_document.doi_url
+    @total_file_size = solr_document.total_file_size
+    @embargo_date = solr_document.embargo_date
+    @globus_url = solr_document.globus_uri
   end
 end
