3d02a5e2502ba4ba236795f273fdf0d453dcdf90
Claudia Lee <claudialee57@gmail.com>
2024-09-25 15:04:29 -0400

    authors helper

--- app/helpers/schema_org_helper.rb
+++ app/helpers/schema_org_helper.rb
@@ -15,6 +15,29 @@ module SchemaOrgHelper
   end
 end
 
+  def authors_helper(authors)
+    html = "["
+    authors.each_with_index do |author, i|
+      if i < (authors.count-1)
+        html += '{<br>"name": ' + html_escape(author.value) + '<br>'
+        html += '"affiliation": ' + html_escape(author.affiliation_name) + '<br>'
+        html += '"identifier": ' + html_escape(author.orcid) + '<br>'
+        html += '},'
+      else
+        html += '{<br>"name": ' + html_escape(author.value) + '<br>'
+        html += '"affiliation": ' + html_escape(author.affiliation_name) + '<br>'
+        html += '"identifier": ' + html_escape(author.orcid) + '<br>'
+        html += '}'
+      end
+    end
+    html += "]"
+    html.html_safe
+  end
+
+
+  # "name": "<%= author.value %>"
+  #             "affiliation": "<%= author.affiliation_name%>"
+  #             "identifier": "<%= author.orcid%>"
 
 # "keywords":
 # [
