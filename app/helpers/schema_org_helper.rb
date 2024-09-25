4d49493f4c7e74a43fdcf81eb1d00408f7d5352b
Claudia Lee <claudialee57@gmail.com>
2024-09-25 17:14:47 -0400

    authors and licenses helper

--- app/helpers/schema_org_helper.rb
+++ app/helpers/schema_org_helper.rb
@@ -10,22 +10,33 @@ module SchemaOrgHelper
   end
 
   def authors_helper(authors)
-    html = "["
-    # TODO: Add logic for affiliation or identifier not being in the author
-    authors.each_with_index do |author, i|
-      if i < (authors.count-1)
-        html += "{\r\n \"name\": " + html_escape(author.value) + "\r\n"
-        html += '"affiliation": ' + html_escape(author.affiliation_name) + '<br>'
-        html += '"identifier": ' + html_escape(author.orcid) + '<br>'
-        html += '},'
-      else
-        html += '{<br>"name": ' + html_escape(author.value) + '<br>'
-        html += '"affiliation": ' + html_escape(author.affiliation_name) + '<br>'
-        html += '"identifier": ' + html_escape(author.orcid) + '<br>'
-        html += '}'
+    authors_json = authors.each.map do |author|
+      json_str = "\n\t\t\t{\n\t\t\t" + '"name": ' + author.value 
+
+      if author.affiliation_name.present?
+        json_str += "\n\t\t\t" + '"affiliation": ' + author.affiliation_name 
       end
+      
+      if author.orcid.present?
+        json_str += "\n\t\t\t" + '"identifier": ' + author.orcid
+      end
+
+      json_str += "\n\t\t\t}"
+      json_str
     end
-    html += "]"
+    html = "[" + authors_json.join(",") + "]"
+    html.html_safe
+  end
+
+  def licenses_helper(licenses)
+    licenses_json = licenses.each.map do |license|
+      "{\n" + 
+      "\t\t\t" + "@type: " + "Dataset" + ",\n" + 
+      "\t\t\t" + "text: " + license['identifier'] + ",\n" + 
+      "\t\t\t" + "url: " + license['uri'] + 
+      "}"
+    end
+    html = "[" + licenses_json.join(",") + "]"
     html.html_safe
   end
 end
