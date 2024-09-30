3c396ed357881d5efdef1f6630d5b08ce8468b11
Claudia Lee <claudialee57@gmail.com>
2024-09-30 14:50:51 -0400

    licenses, partial view, tests

--- app/helpers/schema_org_helper.rb
+++ app/helpers/schema_org_helper.rb
@@ -12,15 +12,12 @@ module SchemaOrgHelper
   def authors_helper(authors)
     authors_json = authors.each.map do |author|
       json_str = "\n\t\t\t{\n\t\t\t" + '"name": ' + '"' + author.value + '"'
-
       if author.affiliation_name.present?
         json_str += "\n\t\t\t" + '"affiliation": ' + '"' + author.affiliation_name + '"'
-      end
-      
+      end  
       if author.orcid.present?
         json_str += "\n\t\t\t" + '"identifier": ' + '"' + author.orcid + '"'
       end
-
       json_str += "\n\t\t\t}"
       json_str
     end
@@ -28,16 +25,17 @@ module SchemaOrgHelper
     html.html_safe
   end
 
-  def licenses_helper(licenses)
-    licenses_json = licenses.each.map do |license|
-      "{\n" + 
-      "\t\t\t" + '"@type:" ' + '"Dataset"' + ",\n" + 
-      "\t\t\t" + '"text:" ' + '"' + license['identifier'] + '"' + ",\n" + 
-      "\t\t\t" + '"url:" ' + '"' + license['uri'] + '"' + 
-      "}"
+  def license_helper(licenses)
+    if licenses.count == 0 
+      ""
+    else
+      html = '"license": {'
+      html += "\n\t\t\t" + '"@type:" ' + '"Dataset"' + ",\n" + 
+        "\t\t\t" + '"text:" ' + '"' + licenses[0]['identifier'] + '"' + ",\n" + 
+        "\t\t\t" + '"url:" ' + '"' + licenses[0]['uri'] + '"'  
+      html += "\n\t\t\t},"
+      html.html_safe
     end
-    html = "[" + licenses_json.join(",") + "]"
-    html.html_safe
   end
 end
 
