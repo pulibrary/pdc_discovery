379974235de36a4c5fe525d8678a2127c64c634a
Claudia Lee <claudialee57@gmail.com>
2024-10-22 00:49:29 -0400

    added hyperlink

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -263,7 +263,6 @@ class CatalogController < ApplicationController
   def show
     @render_links = !agent_is_crawler?
     super
-    byebug
     if params["format"] == "json"
       render json: DocumentExport.new(@document)
     end
