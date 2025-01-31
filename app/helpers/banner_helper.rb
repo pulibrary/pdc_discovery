83eae94a50a061df45362705df927a3359a75acc
Claudia Lee <claudialee57@gmail.com>
2025-01-31 14:50:48 -0500

    commented out and removed data from yml file

--- app/helpers/banner_helper.rb
+++ app/helpers/banner_helper.rb
@@ -6,7 +6,7 @@ module BannerHelper
   # rubocop:disable Rails/ContentTag
   def banner_content
     @yaml_data = YAML.load_file('config/banner.yml')
-    return false if @yaml_data[Rails.env].nil?
+    return false if @yaml_data.nil? || @yaml_data[Rails.env].nil?
     @banner_title = "<h1>#{@yaml_data[Rails.env]['title']}</h1>"
     @banner_body = "<p>#{@yaml_data[Rails.env]['body']}</p>"
   end
