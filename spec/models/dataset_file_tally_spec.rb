0d6b19e5a53cd72546dd448a63b018d3cb8e6e2c
Hector Correa <hector_correa@princeton.edu>
2025-04-03 09:43:24 -0400

    Adjust test to better detect the file information regardless of the order of the data

--- spec/models/dataset_file_tally_spec.rb
+++ spec/models/dataset_file_tally_spec.rb
@@ -12,6 +12,7 @@ RSpec.describe DatasetFileTally do
 
   let(:dft) { described_class.new(timestamp) }
   let(:timestamp) { Time.zone.parse("2025-03-25 18:01") }
+  let(:file_info_line) { "doi-10-34770-r75s-9j74,bitKlavier Grand Sample Library—Binaural Mic Image,2021,4,1108910,folder-a/file3.txt,396003,https" }
 
   it 'has a timestamp' do
     expect(dft.timestamp.year).to eq Time.now.in_time_zone.year
@@ -33,23 +34,20 @@ RSpec.describe DatasetFileTally do
       lines = File.readlines(dft.filepath)
       expect(lines.count).to be 3
       expect(lines[0]).to eq "id,title,issue_date,file_count,total_file_size\n"
-      expect(lines[1]).to eq "doi-10-34770-00yp-2w12,Sowing the Seeds for More Usable Web Archives: A Usability Study of Archive-It,2019,2,99703\n"
+      expect(lines[1]).to eq "doi-10-34770-00yp-2w12,Sowing the Seeds for More Usable Web Archives: A Usability Study of Archive-It,2023,0,0\n"
       expect(lines[2]).to eq "doi-10-34770-r75s-9j74,bitKlavier Grand Sample Library—Binaural Mic Image,2021,4,1108910\n"
     end
   end
 
-  # rubocop:disable Layout/LineLength
   context "#details" do
     let(:timestamp) { Time.zone.parse("2025-03-25 18:03") }
 
     it 'produces an export with the file list included' do
       dft.details
       lines = File.readlines(dft.filepath)
-      expect(lines.count).to be 7
+      expect(lines.count).to eq 5
       expect(lines[0]).to eq "id,title,issue_date,file_count,total_file_size,file_name,file_size,url\n"
-      expect(lines[1].start_with?("doi-10-34770-00yp-2w12,Sowing the Seeds for More Usable Web Archives: A Usability Study of Archive-It,2019,2,99703,Archive-It-UsabilityTestDataAnalysis-2017.xlsx,94578,https")).to be true
-      expect(lines[6].start_with?("doi-10-34770-r75s-9j74,bitKlavier Grand Sample Library—Binaural Mic Image,2021,4,1108910,folder-a/file3.txt,396003,https")).to be true
+      expect(lines.find { |line| line.start_with?(file_info_line) }).to_not be nil
     end
   end
-  # rubocop:enble Layout/LineLength
 end
