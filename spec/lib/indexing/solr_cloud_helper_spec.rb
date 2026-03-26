eb9f2bb7343fae74e13de95259d1876168749b3f
Bess Sadler <bess.sadler@princeton.edu>
2026-03-26 08:13:13 -0400

    Distinguish b/w local and ci server test failures

--- spec/lib/indexing/solr_cloud_helper_spec.rb
+++ spec/lib/indexing/solr_cloud_helper_spec.rb
@@ -57,7 +57,14 @@ RSpec.describe Indexing::SolrCloudHelper do
   end
 
   describe "#collection_writer_for_alias" do
-    it "returns the alternate collection as the collection_writer" do
+
+    # In the *local* test environment, this will encounter a check like this:
+    #       if config_set.nil?
+    #               return solr_alias_uri.to_s
+    #       end
+    # which will cause it to return the provided alias URI as the collection writer 
+    # URL since we are not running in a Solr cloud environment in the local test environment.
+    # However, this passes on the CI server.  
       stub_request(:get, "http://fake-solr/solr/admin/collections?action=LISTALIASES")
         .to_return(status: 200, body: solr_aliases_1, headers: json_response)
       expect(described_class.collection_writer_for_alias(alias_uri, false)).to eq "http://fake-solr/solr/pdc-discovery-staging-2"
