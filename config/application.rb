d2525167e41548a736b945af16dea327bf0e2b35
Bess Sadler <bess.sadler@princeton.edu>
2026-08-28 09:02:37 -0400

    Only write json logs in some environments

--- config/application.rb
+++ config/application.rb
@@ -33,9 +33,10 @@ module PdcDiscovery
     # See https://logger.reidmorrison.com/rails.html for more
     # We want a .log file, because that's fast, and a .json file,
     # because that's structured and can be parsed by signoz.
+    environments_needing_signoz = %w[production staging]
     config.rails_semantic_logger.appenders do |appenders|
       appenders.add(file_name: "log/#{Rails.env}.log", formatter: :color)
-      appenders.add(file_name: "log/#{Rails.env}.json", formatter: :json)
+      appenders.add(file_name: "log/#{Rails.env}.json", formatter: :json) if environments_needing_signoz.include?(Rails.env)
     end
 
     config.pdc_discovery = config_for(:pdc_discovery)
