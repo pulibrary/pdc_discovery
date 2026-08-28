2807cbba9a362c870b057e53e54ef1d327c93dd6
Bess Sadler <bess.sadler@princeton.edu>
2026-08-28 08:47:20 -0400

    Configure a separate .json log file for signoz

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,15 @@ module PdcDiscovery
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    # Use semantic logger for Rails logging
+    # See https://logger.reidmorrison.com/rails.html for more
+    # We want a .log file, because that's fast, and a .json file, 
+    # because that's structured and can be parsed by signoz.
+    config.rails_semantic_logger.appenders do |appenders|
+      appenders.add(file_name: "log/#{Rails.env}.log", formatter: :color)
+      appenders.add(file_name: "log/#{Rails.env}.json", formatter: :json)
+    end
+
     config.pdc_discovery = config_for(:pdc_discovery)
   end
 end
